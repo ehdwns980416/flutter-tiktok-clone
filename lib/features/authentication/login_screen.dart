eee938e7eb2e440e7477bee88fb6ac6975bad493
ehdwns0416 <end75814@gmail.com>
2023-01-15 20:59:49 +0900

    Formdata with validation

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/login_form_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -42,6 +43,7 @@ class LoginScreen extends StatelessWidget {
               AuthButton(
                 text: 'Use email & password',
                 icon: FaIcon(FontAwesomeIcons.user),
+                targetScreen: LoginFormScreen(),
               ),
               Gaps.v16,
               AuthButton(
